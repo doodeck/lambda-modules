2ba1b916ec2ef47e2f81f9ffa7c7d2c3d9bb89d6
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2015-05-02 17:44:51 +0200

    Testing what is undefined

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var modularize = require('./modules/module.js').modularize;
 exports.handler = function(event, context) {
     console.log('value1 =', event.key1);
     console.log('value2 =', event.key2);
+    console.log('modularize: ', modularize);
     console.log('modularize: ', modularize('Name'));
     console.log('value3 =', event.key3);
     context.succeed(event.key1);  // Echo back the first key value
